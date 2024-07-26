50c901dc26f845e6958f34a7f269ef947dd9bf21
Saulo Coelho <coelho.saulo@gmail.com>
2024-07-26 18:33:51 -0300

    changed MainActivity.java

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
-import Test from './src/paginas/Teste';
+import Navigation from './src/rotas/navigation';
+
+import {Teste} from "../Adopets/src/paginas/Teste"/
 
 const App = () => {
   return (
     <SafeAreaView style={estilos.tela}>
-      <Test />
+      {/* <Navigation /> */}
+      <Teste/>
     </SafeAreaView>
   );
 };
